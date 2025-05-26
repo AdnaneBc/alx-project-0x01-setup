7d82441d7f92770d314d104d93294031603d0897
Adnane BC <adnane.bachchar@gmail.com>
2025-05-26 15:30:24 +0100

    add Header

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Posts;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Users;
