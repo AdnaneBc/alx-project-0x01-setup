67b1668b29529bfb08207e520987c1e68d662289
Adnane BC <adnane.bachchar@gmail.com>
2025-05-26 20:56:31 +0100

    add Header

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -11,3 +11,13 @@ const Users = () => {
 };
 
 export default Users;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
